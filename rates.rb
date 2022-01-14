006ffd3ed4dd9b84dfc390ce68ef640449a4cc42
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2022-01-14 14:02:24 +0100

    refactoring

--- rates.rb
+++ rates.rb
@@ -32,47 +32,51 @@ class Calendar
   end
 end
 
-def current_value calendar, ir
-  monthly_interest_rate = ir / 12.0
-  calendar.reduce 0 do |acc, payment|
-    acc * (1 + monthly_interest_rate) + payment
+class Calculator
+
+  def current_value calendar, ir
+    monthly_interest_rate = ir / 12.0
+    calendar.reduce 0 do |acc, payment|
+      acc * (1 + monthly_interest_rate) + payment
+    end
   end
-end
 
-def interest_rate calendar, cv
-  ir = 1.0
-  ir_upper = ir_lower = nil
-  while true do
-    value = current_value calendar, ir
+  def interest_rate calendar, cv
+    ir = 1.0
+    ir_upper = ir_lower = nil
+    while true do
+      value = current_value calendar, ir
 
-    if value > cv then
-      ir_upper = ir
-    end
-    if value < cv then
-      ir_lower = ir
-    end
-    
-    if ir_lower && ir_upper then
-      if ir_lower.round(PRECISION) == ir_upper.round(PRECISION) then
-        return ir
+      if value > cv then
+        ir_upper = ir
       end
       if value < cv then
-        ir = (ir + ir_upper) / 2.0
-      elsif value > cv then
-        ir = (ir_lower + ir) / 2.0
-      else
-        return ir
+        ir_lower = ir
       end
-    elsif ir_lower then
-      ir = 2.0 * ir_lower
-    elsif ir_upper then
-      ir = ir_upper / 2.0
-    else
-      throw "At least one of ir_lower, ir_upper should always be known."
+      
+      if ir_lower && ir_upper then
+        if ir_lower.round(PRECISION) == ir_upper.round(PRECISION) then
+          return ir
+        end
+        if value < cv then
+          ir = (ir + ir_upper) / 2.0
+        elsif value > cv then
+          ir = (ir_lower + ir) / 2.0
+        else
+          return ir
+        end
+      elsif ir_lower then
+        ir = 2.0 * ir_lower
+      elsif ir_upper then
+        ir = ir_upper / 2.0
+      else
+        throw "At least one of ir_lower, ir_upper should always be known."
+      end
     end
   end
 end
 
+calculator = Calculator.new
 calendar, current_value = parse_args ARGV
-yearly_interest_rate = interest_rate calendar, current_value
+yearly_interest_rate = calculator.interest_rate calendar, current_value
 puts "Interest rate: #{(yearly_interest_rate * 100).round(1)}% p.a."
